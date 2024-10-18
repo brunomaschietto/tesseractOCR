a2c2ec6160ced4b76d42e05c4c375cd25aa42c78
brunomaschietto <bruno4_10@hotmail.com>
2024-10-18 13:52:06 -0300

    App mudado

--- app.py
+++ app.py
@@ -61,4 +61,4 @@ def extract_text():
     return jsonify({'text': full_text, 'tamanho': tamanho})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5001)
