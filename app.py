36481beec168e936d4ed5cb4dcf23134cc90a368
brunomaschietto <bruno4_10@hotmail.com>
2024-10-24 13:18:59 -0300

    Mudança pra nao repartir

--- app.py
+++ app.py
@@ -8,11 +8,7 @@ import unicodedata
 
 app = Flask(__name__)
 CORS(app, resources={r"/extract_text": {"origins": "https://www.legnet.com.br"}}) 
-app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024  # Limite de 100 MB
-
-UPLOAD_FOLDER = 'uploads'
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
+app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
 
 def normalize_text(text):
     return unicodedata.normalize('NFKD', text)
@@ -28,35 +24,38 @@ def hello_world():
 
 @app.route('/extract_text', methods=['POST'])
 def extract_text():
-    if 'pdf' not in request.files:
-        return jsonify({'error': 'Nenhum arquivo foi enviado.'}), 400
-
     file = request.files['pdf']
-    file.save(os.path.join(UPLOAD_FOLDER, 'input.pdf'))
+    file.save('input.pdf')
 
-    # Converte o PDF em imagens
-    images = convert_from_path(os.path.join(UPLOAD_FOLDER, 'input.pdf'), 500)
+    # No need to specify poppler_path in Linux
+    images = convert_from_path('input.pdf', 500)
+
+    # No need to set the tesseract_cmd in Linux
+    # pytesseract.pytesseract.tesseract_cmd = "C:\Program Files\Tesseract-OCR\Tesseract.exe"
+
     pages_text = []
 
+    # Config without tessdata_dir_config for Linux
     config = r'--oem 3 --psm 6'
 
-    for i, image in enumerate(images):
-        # Processa cada página
+    for i in range(len(images)):
+        image_path = 'page' + str(i) + '.jpg'
+        images[i].save(image_path, 'JPEG')
+
+        image = cv2.imread(image_path)
         processed_image = preprocess_image(image)
 
-        # Converte a imagem em texto
         text = pytesseract.image_to_string(processed_image, config=config, lang='por')
         normalized_text = normalize_text(text)
         pages_text.append(normalized_text)
 
-        # Aqui você pode querer dividir o texto em pedaços, se necessário
-        # Se o texto for muito grande, você pode armazenar em uma lista ou apenas
-        # dividir em partes menores
-        if len(pages_text) > 0 and len(pages_text[-1]) > 10000:  # Exemplo: dividir se maior que 10.000 caracteres
-            pages_text[-1] = pages_text[-1][:10000]  # Limita a 10.000 caracteres
-            pages_text.append("...")  # Indicador de que o texto foi cortado
+        try:
+            os.remove(image_path)
+            print(f"Imagem {image_path} apagada com sucesso.")
+        except OSError as e:
+            print(f"Erro ao apagar a imagem {image_path}: {e}")
 
-    os.remove(os.path.join(UPLOAD_FOLDER, 'input.pdf'))  # Remove o PDF original após o processamento
+    os.remove('input.pdf')
     full_text = ' '.join(pages_text)
     tamanho = len(full_text)
 
